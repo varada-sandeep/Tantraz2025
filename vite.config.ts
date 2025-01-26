46ae79ffb968b31c5e88e51ac2221f033b588dd5
SANDEEP VARADA <sandeepvarada4@gmail.com>
2025-01-26 23:26:46 +0530

    made small changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Tantraz2025',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
